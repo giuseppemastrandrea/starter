b96e1abd0027bd1941ffc17383d1f5659ee614f8
Giuseppe Mastrandrea <giumast@Mac-mini-di-macmini255.fritz.box>
2021-04-23 12:58:29 +0200

    Lezione del 23/04/2021

--- aliases.js
+++ aliases.js
@@ -7,6 +7,8 @@ const obj = {
 		"@fonts": path.resolve(__dirname, 	'src/fonts'),
 		"@js": path.resolve(__dirname, 	'src/js'),
 		"@styles": path.resolve(__dirname, 	'src/styles'),
+		"@less": path.resolve(__dirname, 	'src/less'),
+		"@scss": path.resolve(__dirname, 	'src/scss'),
 	}
 }
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,6 @@
 import moment from 'moment'
 
+
+import '../css/grid.css'
+
 console.log(moment)
